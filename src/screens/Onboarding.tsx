af3dafd6821551a2f21d01e239982422021225dd
pdev6373 <pdev6373@gmail.com>
2024-02-12 19:01:47 +0100

    working on onboarding

--- src/screens/Onboarding.tsx
+++ src/screens/Onboarding.tsx
@@ -11,7 +11,6 @@ import {Data} from '../constants';
 import {useRef, useState} from 'react';
 import {Bottom, Button, Text} from '../components';
 import {ButtonStylesType, TextType} from '../../types';
-import {useNavigation} from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type OnboardingStylesType = {
